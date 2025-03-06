48efcafb95c9c769a49a6dccadb619b6172c43a9
Juan Pablo Gil <juanpablo.gil@icloud.com>
2025-03-06 14:03:24 -0600

    Remove Railtie support and associated configuration task from Navio

--- lib/navio.rb
+++ lib/navio.rb
@@ -4,7 +4,6 @@ require_relative "navio/version"
 require_relative "navio/config"
 require_relative "navio/launcher"
 require_relative "navio/cli"
-require_relative "navio/railtie"
 
 # Navio is a module that provides functionality for the Navio application.
 module Navio
